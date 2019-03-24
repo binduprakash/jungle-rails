02413a21be8b7024ebd1295325e7218f70888fbc
Bindu Prakash <bindu2305@gmail.com>
2019-03-24 22:16:57 +0000

    Completed Step 11: reviews create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,25 @@
 class ReviewsController < ApplicationController
     def create
-        raise "Yay, I'm here!"
+        @review = Review.new(review_params)
+        print @review.valid?
+
+        @review.save
+        redirect_to :back
+    end
+    def destroy
+        @review = Review.find(params[:id])
+        if (current_user && current_user.id == @review.user.id)
+            @review.destroy
+        end
+        redirect_to :back
+    end
+    private
+    def review_params
+        params.require(:review).permit(
+            :description,
+            :rating,
+            :product_id,
+            :user_id
+        )
     end
 end
