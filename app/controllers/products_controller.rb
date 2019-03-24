1f7232fdbae8cfb7fbf605877834f9fdc710119e
Bindu Prakash <bindu2305@gmail.com>
2019-03-24 22:17:18 +0000

    Completed Step: 12 & 13

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,15 @@
 class ProductsController < ApplicationController
+  before_filter :find_product, :only => [:show]
 
   def index
     @products = Product.all.order(created_at: :desc)
   end
 
   def show
-    @product = Product.find params[:id]
     @review = Review.new
   end
-
+  protected
+    def find_product
+      @product = Product.find(params[:id])
+    end
 end
